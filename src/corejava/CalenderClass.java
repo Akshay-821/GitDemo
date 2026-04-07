26157d60456805e2f4407f78a530bb17f8d32cfa
akshay <akshaydalvi821@gmail.com>
2026-04-08 03:02:43 +0530

    American architech commit

--- src/corejava/CalenderClass.java
+++ src/corejava/CalenderClass.java
@@ -2,8 +2,23 @@ package corejava;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class CalenderClass {
+	
+	public void date() {
+		Date d=new Date();
+		//to pring normal Date like (Wed Mar 18 20:24:08 IST 2026)
+		System.out.println(d.toString()); 
+		
+		//to print normal Date in different format
+		SimpleDateFormat sdf=new SimpleDateFormat("M/dd/YYYY");
+		System.out.println(sdf.format(d));
+		
+		//to print Date with time in different format
+		SimpleDateFormat sd=new SimpleDateFormat("M/dd/YYYY hh:mm:ss");
+		System.out.println(sd.format(d));
+	}
 
 	public static void main(String[] args) {
 		
